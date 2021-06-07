66e68fb0893bda50df9e1de0bdc29fc4585b8eb1
StephenHwang <sjhwang@ucsc.edu>
2021-06-07 03:03:57 -0700

    Derive first signature matrix.

--- Cibersort.R
+++ Cibersort.R
@@ -26,46 +26,42 @@ source('bin/CIBERSORT.R')
 #   Dendritic cells
 
 
-# sig.matrix <-"signature_matrix/LM5.txt"        # 550 genes
-# sig.matrix <-"signature_matrix/holiday.txt"    # too many genes, takes too long to run (12366)
+#sig.matrix <-"signature_matrix/LM5.txt"        # 550 genes
+#sig.matrix <-"signature_matrix/holiday.txt"    # too many genes, takes too long to run (12366)
+#sig.matrix <-"signature_matrix/LM_joint.txt"    # not as good at LM5
 
-sig.matrix <-"signature_matrix/LM_joint.txt"    # too many genes, takes too long to run (12366)
+sig.matrix <-"signature_matrix/signature_1.txt"
+View(read.table(sig.matrix, header=T, sep="\t", row.names=1, check.names=F))
+
 mixture.matrix <- "train_bulk_mixture_data/CellLineA_Tumor_bulk_composition_noise_0.tsv"
 
 results <- CIBERSORT(sig.matrix, mixture.matrix, perm=100, QN=FALSE, absolute=TRUE, abs_method='no.sumto1')
 write.table(results,file="./Cibersort_ExampleOutput.tsv",sep = "\t", quote = F,row.names = T, col.names = T) # write result table to file
 # 1:21 -
 
-# join columns and re-name
-merged_results <- cbind(
-rowSums(results[,  c("B cells naive", "B cells memory" )]),
-rowSums(results[, c("T cells CD8",
-            "T cells CD4 naive",
-            "T cells CD4 memory resting",
-            "T cells CD4 memory activated",
-            "T cells follicular helper",
-            "T cells regulatory (Tregs)",
-            "T cells gamma delta")]),
-rowSums(results[, c("NK cells resting", "NK cells activated")]),
-results[, c("Monocytes")],
-rowSums(results[, c("Dendritic cells resting", "Dendritic cells activated")])
-)
-colnames(merged_results) <- c("avg_B.cell", "avg_T.cell", "avg_NK.cell", "avg_Monocyte", "avg_Dendritic.cell")
-
-
-P-value	Correlation	RMSE	Absolute score (no.sumto1)
-
-
+## LM22_joint: join columns and re-name
+# merged_results <- cbind(
+# rowSums(results[,  c("B cells naive", "B cells memory" )]),
+# rowSums(results[, c("T cells CD8",
+#             "T cells CD4 naive",
+#             "T cells CD4 memory resting",
+#             "T cells CD4 memory activated",
+#             "T cells follicular helper",
+#             "T cells regulatory (Tregs)",
+#             "T cells gamma delta")]),
+# rowSums(results[, c("NK cells resting", "NK cells activated")]),
+# results[, c("Monocytes")],
+# rowSums(results[, c("Dendritic cells resting", "Dendritic cells activated")])
+# )
+# colnames(merged_results) <- c("avg_B.cell", "avg_T.cell", "avg_NK.cell", "avg_Monocyte", "avg_Dendritic.cell")
+# deconv.results <- merged_results
 
 # View results
 results
 
-test.mixture.matrix.7 <- 'test_bulk_mixture_data/bulk_composition_from_scRNA-seq/bulk_composition_7.tsv'
-test.mixture.matrix.8 <- 'test_bulk_mixture_data/bulk_composition_from_scRNA-seq/bulk_composition_8.tsv'
 
 
-# testing
-X <- read.table(sig.matrix, header=T, sep="\t", row.names=1, check.names=F)
+
 
 
 ################################################################################
@@ -87,8 +83,6 @@ cells <- c("T","B","M", "NK", "Dendritic")
 #pdf("Cibersort_ExampleOutput_plot.pdf", width=6, height=12)
 #par(mfrow=c(6,3), mar=c(3,3,3,3))  #arrange the plot in pdf and set the margin
 deconv.results <- read.delim(file)
-
-deconv.results <- merged_results
 
 #load cell proportions from mixture file
 true.T.prop <- unlist(lapply(rownames(deconv.results), function(x) as.numeric(unlist(strsplit(x, split="::", fixed=T))[12])/100))
@@ -117,5 +111,23 @@ for (cell in cells){
   Corner_text(paste("RMSE=", round(mean(na.omit(unlist(lapply(1:200, function(i) RMSE(pbmc.results[,paste(c(cell,".result"), collapse="")][i], pbmc.results[,paste(c("true.",cell,".prop"), collapse="")][i]))))),3), sep=" "),location="bottomright")
 }
 #dev.off()
+
+
+
+# Overall results
+matrix1 <- pbmc.results[, 1:5]
+matrix2 <- pbmc.results[, 6:10]
+results.cor <- cor(c(matrix1), c(matrix2), method='pearson')
+results.RMSE <- RMSE(c(matrix1), c(matrix2))
+
+cat(paste(paste('Sig:', gsub('signature_matrix/', '', sig.matrix)),
+      paste('cor:', results.cor),
+      paste('RMSE:', results.RMSE),
+      sep='\n'))
+
+
+
+
+
 
 
